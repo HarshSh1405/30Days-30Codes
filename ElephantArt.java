2e2961d8ac2cf3fb5c1cc42605eb1ebaf6e75790
HarshSh1405 <144427747+HarshSh1405@users.noreply.github.com>
2024-02-15 22:13:51 +0530

    Add files via upload

---
+++ ElephantArt.java
@@ -0,0 +1,12 @@
+public class ElephantArt {
+    public static void main(String[] args) {
+        System.out.println("  __  __");
+        System.out.println(" (o \\/ o)");
+        System.out.println("  \\  =  /");
+        System.out.println(" /`---'\\");
+        System.out.println("/         \\");
+        System.out.println("|         |");
+        System.out.println(" \\       /");
+        System.out.println("  \\__ __/");
+    }
+}
